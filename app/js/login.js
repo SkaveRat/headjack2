72062eaaa346e6c499148fbb6026848fb72b69db
SkaveRat <skaverat@funlynx.de>
2015-05-06 22:28:47 +0200

    adding disabled on form submit

--- app/js/login.js
+++ app/js/login.js
@@ -1,10 +1,14 @@
 headjackApp.controller('LoginCtrl', ['$scope', '$http', 'matrixService', function ($scope, $http, matrixService) {
 
     $scope.loginform = {
+        disabled: false,
         type: 'm.login.password'
     };
 
     $scope.login = function() {
+        $scope.loginform.disabled = true;
+
+
         chrome.runtime.sendMessage({
             type: 'login',
             loginform: $scope.loginform
