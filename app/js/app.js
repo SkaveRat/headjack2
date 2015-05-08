366300dd9b6643945241bb110d5fb18a36d0d058
SkaveRat <skaverat@funlynx.de>
2015-05-08 20:55:04 +0200

    material design contactlist

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,9 @@
 var headjackApp = angular.module('headjackApp', ['ngResource', 'matrixService', 'chmsg']);
+headjackApp.filter('keylength', function(){
+    return function(input){
+        if(!angular.isObject(input)){
+            throw Error("Usage of non-objects with keylength filter!!")
+        }
+        return Object.keys(input).length;
+    }
+});
